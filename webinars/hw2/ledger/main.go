ba83c528ad5e584371a49b25050a562913803295
Aleksey Sidorov <you@example.com>
2026-06-28 14:17:35 +0300

    HW4 ledger with interfaces

--- webinars/hw2/ledger/main.go
+++ webinars/hw2/ledger/main.go
@@ -2,30 +2,10 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 	"time"
 )
-
-type Transaction struct {
-	ID          int
-	Amount      float64
-	Category    string
-	Description string
-	Date        time.Time
-}
-
-type Budget struct {
-	Category string
-	Limit    float64
-	Period   string
-}
-
-var transactions []Transaction
-
-var budgets map[string]map[string]Budget
 
 func main() {
 	handleBudget(Budget{Category: "Food", Limit: 5000, Period: "2026"})
@@ -33,6 +13,10 @@ func main() {
 	handleBudget(Budget{Category: "Entertainment", Limit: 3000, Period: "2026"})
 	handleBudget(Budget{Category: "Entertainment", Limit: 4004, Period: "2026"})
 	handleBudget(Budget{Category: "Food", Limit: 7000, Period: "2027"})
+	handleBudget(Budget{Category: "", Limit: 7000, Period: "2027"})
+	handleBudget(Budget{Category: "Food", Limit: -3, Period: "2027"})
+	handleBudget(Budget{Category: "Food", Limit: 7000})
+	handleBudget(Budget{Limit: 7000, Period: "2027"})
 
 	file, err := os.Open("budgets.json")
 	if err != nil {
@@ -57,6 +41,8 @@ func main() {
 
 	handleAdd(Transaction{Amount: 100.0, Category: "Food", Description: "Lunch", Date: time.Now()})
 	handleAdd(Transaction{Amount: 250.0, Category: "Food", Description: "Breakfast on Gili", Date: time.Now()})
+	handleAdd(Transaction{Amount: 250.0, Category: "Food", Description: "Breakfast on Gili 2"})
+	handleAdd(Transaction{Amount: 250.0, Category: "", Description: "Breakfast on Gili 3", Date: time.Now()})
 	handleAdd(Transaction{Amount: 50.0, Category: "Transport", Description: "Taxi", Date: time.Now()})
 	handleAdd(Transaction{Amount: 0.0, Category: "Entertainment", Description: "Movie", Date: time.Now()})
 	handleAdd(Transaction{Amount: 111.0, Category: "Entertainment", Description: "Example", Date: time.Now().AddDate(-1, 0, 0)})
@@ -65,97 +51,4 @@ func main() {
 	handleAdd(Transaction{Amount: 500, Category: "Food", Description: "Extra meal", Date: time.Now()})
 
 	fmt.Println(ListTransactions())
-}
-
-func handleAdd(tx Transaction) {
-	err := AddTransaction(tx)
-	if err != nil {
-		fmt.Println("Ошибка добавления транзакции:", err)
-		return
-	}
-
-	fmt.Println("Успешно добавлена транзакция:", tx.Category, tx.Amount)
-}
-
-func handleBudget(b Budget) {
-	err := SetBudget(b)
-	if err != nil {
-		fmt.Println("Ошибка создания бюджета:", err)
-		return
-	}
-	fmt.Println("Бюджет успешно создан:", b.Category, b.Period)
-}
-
-func AddTransaction(tx Transaction) error {
-
-	if tx.Amount == 0 {
-		return fmt.Errorf("Значение транзакции не может быть равно нулю")
-	}
-	sum := tx.Amount
-	year := tx.Date.Format("2006")
-
-	for _, existingTx := range transactions {
-		if existingTx.Category == tx.Category &&
-			existingTx.Date.Format("2006") == year {
-			sum += existingTx.Amount
-		}
-	}
-	yearBudget := tx.Date.Format("2006")
-
-	budget, exists := budgets[yearBudget][tx.Category]
-
-	if exists {
-		if sum > budget.Limit {
-			return fmt.Errorf("Превышен лимит бюджета для категории: %s", tx.Category)
-		}
-
-		tx.ID = len(transactions) + 1
-		transactions = append(transactions, tx)
-		return nil
-	}
-	return fmt.Errorf("бюджет для года %s в категории %s не предусмотрен", year, tx.Category)
-}
-
-func ListTransactions() []Transaction {
-	return transactions
-}
-
-func SetBudget(budget Budget) error {
-	if budget.Limit == 0 {
-		return fmt.Errorf("Значение бюджета не может быть равно нулю")
-	}
-	year := budget.Period
-
-	if budgets == nil {
-		budgets = make(map[string]map[string]Budget)
-		if budgets[year] == nil {
-			budgets[year] = make(map[string]Budget)
-		}
-		budgets[year][budget.Category] = budget
-		fmt.Println("Бюджет успешно обновлен: ", budget.Category, budget.Period)
-		return nil
-	}
-	if budgets[year] == nil {
-		budgets[year] = make(map[string]Budget)
-	}
-	budgets[year][budget.Category] = budget
-	fmt.Println("Бюджет успешно обновлен: ", budget.Category, budget.Period)
-	return nil
-}
-
-func LoadBudgets(r io.Reader) error {
-	var listBudgets []Budget
-
-	err := json.NewDecoder(r).Decode(&listBudgets)
-	if err != nil {
-		return fmt.Errorf("Ошибка чтения бюджетов из JSON: %w", err)
-	}
-
-	for _, budget := range listBudgets {
-		err := SetBudget(budget)
-		if err != nil {
-			return fmt.Errorf("ошибка установки бюджета %s %s: %w", budget.Category, budget.Period, err)
-		}
-	}
-	return nil
 }
